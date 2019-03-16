9714686125f331b350f4207687b5c3b93f3ff979
Daniel Simão <rui.daniel.simao@gmail.com>
2019-03-16 22:12:13 +0000

    Added CRUD resolvers for User and needed schemas. Started login and signup.

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,4 +1,84 @@
 //TODO type input schemas
+//TODO login and signup
+
+// async function signup(parent, {password}, {db}, info) {
+//   const password = await bcrypt.hash(password, 10);
+
+//   const user = await db.user.createUser({ ...args, password });
+
+//   const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+//   return {
+//     token,
+//     user
+//   };
+// }
+
+// async function login(parent, { username }, { db }, info) {
+//   const user = await db.user({ username });
+//   if (!user) {
+//     throw new Error("No such user found");
+//   }
+
+//   const valid = await bcrypt.compare(args.password, user.password);
+//   if (!valid) {
+//     throw new Error("Invalid password");
+//   }
+
+//   const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+//   return {
+//     token,
+//     user
+//   };
+// }
+
+function createUser(
+  parent,
+  { username, fullname, email, password },
+  { db },
+  info
+) {
+  const user = db.user.create({
+    username,
+    fullname,
+    email,
+    password
+  });
+
+  return user;
+}
+function updateUser(
+  parent,
+  { username, fullname, email, password, id },
+  { db },
+  info
+) {
+  const user = db.user.update(
+    {
+      username,
+      fullname,
+      email,
+      password
+    },
+    {
+      where: {
+        id
+      }
+    }
+  );
+
+  return user;
+}
+function deleteUser(parent, { id }, { db }, info) {
+  const user = db.user.destroy({
+    where: {
+      id: id
+    }
+  });
+
+  return user;
+}
 
 function createRepo(parent, { name, url, balance, userId }, { db }, info) {
   const repo = db.repo.create({
@@ -36,4 +116,13 @@ function deleteRepo(parent, { id }, { db }, info) {
   return repo;
 }
 
-module.exports = { createRepo, updateRepo, deleteRepo };
+module.exports = {
+  // signup,
+  // login,
+  createUser,
+  updateUser,
+  deleteUser,
+  createRepo,
+  updateRepo,
+  deleteRepo
+};
